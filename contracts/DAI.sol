f96a1f5b11d3a29692f397046572476794fac7f0
Maxim <mnovoseltsev@gmail.com>
2018-05-14 15:35:52 +0200

    Mock DAI

--- contracts/DAI.sol
+++ contracts/DAI.sol
@@ -7,7 +7,7 @@ pragma solidity ^0.4.19;
 
 import 'zeppelin-solidity/contracts/token/ERC20/StandardToken.sol';
 
-contract Weth is StandardToken {
+contract DAI is StandardToken {
   string constant public name = "DAI";
   string constant public symbol = "DAI";
   uint8 constant public decimals = 18;
--- migrations/2_token_option.js
+++ migrations/2_token_option.js
@@ -2,10 +2,15 @@ var MockToken1 = artifacts.require("MockToken1")
 var MockToken2 = artifacts.require("MockToken2")
 var TokenOption = artifacts.require("TokenOption")
 var TokenAntiOption = artifacts.require("TokenAntiOption")
+var Weth = artifacts.require("Weth")
+var Dai = artifacts.require("DAI")
+
 
 module.exports = function(deployer) {
   deployer.deploy(MockToken1)
   .then( () => deployer.deploy(MockToken2))
   .then( () => deployer.deploy(TokenOption))
+  .then( () => deployer.deploy(Weth))
+  .then( () => deployer.deploy(Dai))
   .then( () => deployer.deploy(TokenAntiOption))
 }
