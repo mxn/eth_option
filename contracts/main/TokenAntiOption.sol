0e98a1971f75b36d9185e480d8d01ea5de0fc86f
Maxim <mnovoseltsev@gmail.com>
2018-09-07 09:00:01 +0200

    name, sym, decimals

--- contracts/main/TokenAntiOption.sol
+++ contracts/main/TokenAntiOption.sol
@@ -3,6 +3,10 @@ pragma solidity ^0.4.18;
 import "./BaseTokenOption.sol";
 
 contract TokenAntiOption is BaseTokenOption {
+  string public constant name = "ERC20 Anti-Option";
+  string public constant symbol = "AOPT";
+  uint8 public constant decimals = 18;
+
   function isAntiOption() public view returns(bool) {
     return true;
   }
