47767fe844483f4fec337fb04edbfb835e9f68ec
Maxim <mnovoseltsev@gmail.com>
2018-09-07 09:07:47 +0200

    pull up decimals to BaseTokenOption

--- contracts/main/TokenOption.sol
+++ contracts/main/TokenOption.sol
@@ -5,7 +5,6 @@ import "./BaseTokenOption.sol";
 contract TokenOption is BaseTokenOption {
   string public constant name = "ERC20 Option";
   string public constant symbol = "OPT";
-  uint8 public constant decimals = 18;
 
   function isAntiOption() public view returns(bool) {
     return false;
